863b9f167b16e902f8dcf382852c74768aee46d8
Jorge Zerpa (zerpacode) <90111873+jorgezerpa@users.noreply.github.com>
2023-02-08 10:13:04 +0000

    add quick projects routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,3 @@
-import 
-
 module.exports = {
     getProjects(){
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,11 @@ router.get('/projects', async(req, res)=>{
     res.send(result)
 })
 
+router.get('/quick-projects', async(req, res)=>{
+    const result = await DBConnection.list('quick_projects')
+    res.send(result)
+})
+
 router.get('/test', async(req, res)=>{
     res.send('you are the best')
 })
